cce397766c3892e0a0d91aaa5df3d853436e972e
Matias D. Frtiz <fritzmatias@gmail.com>
2018-11-17 11:44:19 -0300

    fixing eclipse console environment

--- ops/eclipse.ide.sh
+++ ops/eclipse.ide.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ln -fs ../jdk /java/eclipse/jre
 [ ${1}x != x ] && UID=$1
 [ ${2}x != x ] && GID=$2
 [ ${UID}x == x ] && UID=1000
@@ -12,10 +11,14 @@ export JAVA_HOME=/java/jdk
 export PATH=$PATH:$JAVA_HOME/bin
 
 mkdir -p /home/developer
-echo "developer:x:"$UID":"$GID":Developer,,,:/home/developer:/bin/sh" >> /etc/passwd
+echo "developer:x:"$UID":"$GID":Developer,,,:/home/developer:/bin/bash" >> /etc/passwd
 echo "developer:x:"$UID":" >> /etc/group
 echo "developer ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod 0440 /etc/sudoers
 chown -R $UID:$GID /home/developer
+
+echo 'export JAVA_HOME=/java/jdk' >> /home/developer/.bashrc
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /home/developer/.bashrc
+ln -fs ../jdk /java/eclipse/jre
 
 sudo -u developer -- /java/eclipse/eclipse -data /java/workspace
